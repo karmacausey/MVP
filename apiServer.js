8c9bff71893916cca7f4cd931a193d2c57169c58
karmacausey <95538159+karmacausey@users.noreply.github.com>
2022-05-23 16:40:21 -0400

    built file structure and initialized NPM

---
+++ apiServer.js
@@ -0,0 +1,20 @@
+require("dotenv").config();
+const express = require("express");
+const db = require("./db/conn.js");
+const app = express();
+
+app.use(express.static("public"));
+
+app.get("/api/favorites", (_, res) => {
+  db.query("SELECT * FROM student").then((data) => {
+    res.json(data.rows);
+  }).catch((error)=>{
+    res.json(error);
+  })
+});
+
+app.listen(process.env.PORT, () => {
+    console.log(`connecting to: ${process.env.DATABASE_URL}`);
+    console.log(`listening on Port ${process.env.PORT}`);
+  });
+  
